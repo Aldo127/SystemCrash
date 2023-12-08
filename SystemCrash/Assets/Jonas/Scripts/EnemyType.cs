86e8d543c41189be668cf356fa18f5195b69c50b
MUKILTEO\1620628 <1620628@mukilteo.wednet.edu>
2023-12-08 12:23:26 -0800

    Hacks now fire projectiles. Started on temporary UI.

--- SystemCrash/Assets/Jonas/Scripts/EnemyType.cs
+++ SystemCrash/Assets/Jonas/Scripts/EnemyType.cs
@@ -9,4 +9,10 @@ public class EnemyType : ScriptableObject
     public int maxHealth = 5;
     public int bitValue = 10;
     public int damage = 10;
+    public float speed = 5;
+
+    public string attackType = "melee";
+    public GameObject projectilePrefab;
+    public int attackCooldown = 10;
+    public int lifetime = -1;
 }
